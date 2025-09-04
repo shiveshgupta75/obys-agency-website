83c99ef503583f783e72e2a80a0f20f0bd400f34
Shivesh Gupta <shiveshgptt.1234@gmail.com>
2025-09-04 23:26:12 +0530

    Page1 done

--- script.js
+++ script.js
@@ -40,6 +40,13 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav",{
+    opacity:0
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
 }
 loadingAnimation();
 
